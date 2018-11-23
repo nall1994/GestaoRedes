deab6e7f549d1ab4584add82bb6e2bfb19448deb
Bruno Carvalho <brunocarvalho@MacBook-Pro-de-Bruno.local>
2018-11-23 15:35:48 +0000

    corrigir web

--- TP2/web/server_tp2.js
+++ TP2/web/server_tp2.js
@@ -3,11 +3,11 @@ var http = require('http')
 var express = require('express')
 var pug = require('pug')
 var fs = require('fs')
+var jsonfile = require('jsonfile');
 
 var app = express()
-var number_interfaces = 0
-var returnValues = []
-var returnValue = {ifDescr:"",PhysAddress:"",inOctets:"",outOctets:"",diferenca:""}
+var myServer = http.createServer(app)
+var myBD = '../Database/database.json'
 
 app.get('/w3.css',(req,res)=> {
     res.writeHead(200,{'Content-Type':'text/css'})
@@ -19,97 +19,18 @@ app.get('/w3.css',(req,res)=> {
 })
 
 app.get('/',(req,res) => {
-    var session = snmp.createSession('localhost','public')
-    getNumberInterfaces(session)
-    setTimeout(getSnmpVariables,100,session)
-    console.log(JSON.stringify(returnValues))
-    setTimeout(writeHTML, 500,res);
-    
-    
-
-    console.log(returnValues)
-
-})
-
-function writeHTML(res) {
-    //returnValue['diferenca'] = (parseInt(returnValue['inOctets']) - parseInt(returnValue['outOctets'])).toString()
-    //console.log(returnValues)
-    res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
-    //console.log(JSON.stringify(returnValues))
-    res.write(pug.renderFile('homepage.pug',{lista: returnValues}))
-    res.end() 
-}
-
-function getSnmpVariables(session) {
-
-    console.log('EEEEEEEE: ' + number_interfaces)
-    for(var i = 1; i< number_interfaces+1;i++) {
-        
-        getIndividualInterface(session,i)
-        returnValues.push(returnValue)
-    }
-}
-
-
-function getIndividualInterface(session,i) {
-    var oid = ["1.3.6.1.2.1.2.2.1.2." + i]
-        session.get(oid, (error,varbinds) => {
-            if(error) console.log('Fail: ' + error)
-            else {
-                returnValue['ifDescr'] = varbinds[0].value.toString()
-                //console.log(JSON.stringify(varbinds[0]))
-            }
-        } )
-
-        oid = ["1.3.6.1.2.1.2.2.1.6." + i]
-        session.get(oid, (error,varbinds) => {
-            if(error) console.log('Fail: ' + error)
-            else {
-                var data = JSON.parse(JSON.stringify(varbinds[0].value))
-                var str = ''
-                for(var i = 0; i < data.data.length; i++) {
-                    if(i == (data.data.length - 1)) {
-                        str += data.data[i].toString(16)
-                    } else {
-                        str += data.data[i].toString(16) + ':' 
-                    }   
-                }
-                console.log(str)
-                returnValue['PhysAddress'] = str
-            }
-        } )
-
-        oid = ["1.3.6.1.2.1.2.2.1.10." + i]
-        session.get(oid, (error,varbinds) => {
-            if(error) console.log('Fail: ' + error)
-            else {
-                returnValue['inOctets'] = varbinds[0].value.toString()
-                //console.log(varbinds[0].value.toString())      
-            }
-        } )
-
-        oid = ["1.3.6.1.2.1.2.2.1.16." + i]
-        session.get(oid, (error,varbinds) => {
-            if(error) console.log('Fail: ' + error)
-            else {
-                returnValue['outOctets'] = varbinds[0].value.toString()
-                //console.log(varbinds[0].value.toString())       
-            }
-        } )
-
-}
-
-function getNumberInterfaces(session) {
-    var oid = ["1.3.6.1.2.1.2.1.0"] 
-    session.get(oid,(error,varbinds) => {
-        if(error) console.log('Fail: ' + error)
-        else {
-            number_interfaces = parseInt(varbinds[0].value)
+    jsonfile.readFile(myBD,(erro,dados)=>{
+        res.writeHead(200,{'Content-Type':'text/html'})
+        if(!erro){
+            res.write(pug.renderFile('homepage.pug',{historico: hist}))
+        }
+        else{
+            res.end()
         }
     })
-}
+    res.end()
+})
 
-var myServer = http.createServer(app)
 
 myServer.listen(2000,() => {
     console.log('Servidor à escuta na porta 2000...')
