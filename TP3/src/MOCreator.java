d2918f03a3378d77173bf13e4adafbed66185dfe
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-02-06 15:51:14 +0000

    m

--- TP3/src/MOCreator.java
+++ TP3/src/MOCreator.java
@@ -1,5 +1,6 @@
 import org.snmp4j.agent.mo.MOAccessImpl;
 import org.snmp4j.agent.mo.MOScalar;
+import org.snmp4j.smi.Integer32;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.Variable;
@@ -14,11 +15,18 @@ public class MOCreator {
         return new MOScalar(oid, MOAccessImpl.ACCESS_READ_ONLY, getVariable(value));
     }
 
+    public static MOScalar createWriteRead(OID oid, Object value) {
+        return new MOScalar(oid, MOAccessImpl.ACCESS_READ_WRITE, getVariable(value));
+    }
+
     
 
     private static Variable getVariable(Object value) {
         if(value instanceof String) {
             return new OctetString((String)value);
+        }
+        if(value instanceof Integer) {
+            return new Integer32((Integer) value);
         }
         throw new IllegalArgumentException("Unmanaged Type: " + value.getClass());
     }
--- TP3/src/TestSNMPAgent.java
+++ TP3/src/TestSNMPAgent.java
@@ -6,7 +6,10 @@ public class TestSNMPAgent {
     public SNMPManager client = null;
     public String address;
 
-    static final OID sysDescr = new OID(".1.3.6.1.3.2019");
+    //static final OID sysDescr = new OID(".1.3.6.1.3.2019");
+    static final OID indexParam = new OID("1.3.6.1.3.2019.1.1.0");
+    static final OID nameParam = new OID("1.3.6.1.3.2019.1.2.0");
+    static final OID flagParam = new OID("1.3.6.1.3.2019.1.3.0");
     public static void main(String[] args) throws IOException {
         TestSNMPAgent client = new TestSNMPAgent("udp:127.0.0.1/161");
         client.init();
@@ -30,11 +33,15 @@ public class TestSNMPAgent {
 
         // Register a system description, use one from you product environment
         // to test with
-        agent.registerManagedObject(MOCreator.createReadOnly(sysDescr,"Teste agente ok!"));
+        agent.registerManagedObject(MOCreator.createWriteRead(indexParam,"ubuntu:latest"));
+        agent.registerManagedObject(MOCreator.createWriteRead(nameParam,"terminal do ubuntu, ultima versao"));
+        agent.registerManagedObject(MOCreator.createWriteRead(flagParam,0));
 
         // Setup the client to use our newly started agent
         client = new SNMPManager("udp:127.0.0.1/2001");
-        System.out.println(client.getAsString(sysDescr));
+        System.out.println(client.getAsString(indexParam));
+        System.out.println(client.getAsString(nameParam));
+        System.out.println(client.getAsString(flagParam));
 
     }
 }
